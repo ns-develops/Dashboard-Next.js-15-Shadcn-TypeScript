ac08843d3548f955f52cad4cee36ee94448b9e46
”ns-develops” <test.tt9061030@gmail.com>
2025-04-24 22:58:24 +0200

    color theme working, now solving last html body issue

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { ThemeProvider } from 'next-themes'; 
-import { Geist, Geist_Mono } from "next/font/google"; 
-import { useTheme } from 'next-themes'; 
-import "./globals.css"; 
+import { useEffect, useState } from "react";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import { ActiveThemeProvider } from "@/components/active-theme";
+import { cn } from "@/lib/utils";
+import Cookies from "js-cookie";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -17,23 +20,49 @@ const geistMono = Geist_Mono({
 
 export default function RootLayout({
   children,
-}: Readonly<{
+}: {
   children: React.ReactNode;
-}>) {
-  const { theme } = useTheme();
+}) {
+  const [activeThemeValue, setActiveThemeValue] = useState<string | undefined>();
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    const themeFromCookie = Cookies.get("active_theme");
+    setActiveThemeValue(themeFromCookie);
+    setMounted(true);
+  }, []);
+
+  const isScaled = activeThemeValue?.endsWith("-scaled");
+
+
+  if (!mounted) return null;
 
   return (
-    <html lang="en" className={theme} style={{ colorScheme: theme }}>
+    <html
+      lang="en"
+      className={activeThemeValue ? `theme-${activeThemeValue}` : ""}
+      style={{
+        colorScheme: activeThemeValue?.includes("dark") ? "dark" : "light",
+      }}
+      suppressHydrationWarning
+    >
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={cn(
+          "bg-background overscroll-none font-sans antialiased",
+          activeThemeValue ? `theme-${activeThemeValue}` : "",
+          isScaled ? "theme-scaled" : ""
+        )}
       >
         <ThemeProvider
           attribute="class"
           defaultTheme="system"
           enableSystem
           disableTransitionOnChange
+          enableColorScheme
         >
-          {children}
+          <ActiveThemeProvider initialTheme={activeThemeValue}>
+            {children}
+          </ActiveThemeProvider>
         </ThemeProvider>
       </body>
     </html>
--- components/site-header.tsx
+++ components/site-header.tsx
@@ -1,6 +1,7 @@
 import { Separator } from "@/components/ui/separator"
 import { SidebarTrigger } from "@/components/ui/sidebar"
 import { ModeToggle } from "./ui/mode-toggle"
+import { ThemeSelector } from "./theme-selector"
 
 export function SiteHeader() {
   return (
@@ -13,6 +14,7 @@ export function SiteHeader() {
         />
         <h1 className="text-base font-medium">Documents</h1>
         <div className="ml-auto flex items-center gap-2">
+        <ThemeSelector/>
         <ModeToggle/>
         </div>
       </div>
